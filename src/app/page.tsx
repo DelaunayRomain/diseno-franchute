da89d9b42ad878a90e3d358fb17f675e78fe444e
DelaunayRomain <disenofranchute@gmail.com>
2025-10-11 14:38:16 -0300

    fetch furnitures from DB in Gallery

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,60 +1,15 @@
-'use client'
-
 import * as React from 'react'
 
-import { About } from '../components/about'
-import { FixedContentWrapper } from '../components/fixed_content_wrapper'
-import { Gallery } from '../components/gallery'
-import { SwitchContentButton } from '../components/switch_content_button'
-import { cn } from '../utils/tailwind'
+import { PrismaClient } from '@prisma/client'
 
-type Content = 'gallery' | 'about'
+import { MainContent } from '../components/main_content'
 
-const Home = () => {
-  const [initialState, setInitialState] = React.useState(true)
-  const [currentContent, setCurrentContent] = React.useState<Content>('gallery')
-  const [disabledButton, setDisabledButton] = React.useState(false)
+const prisma = new PrismaClient()
 
-  const disableButton = () => {
-    setDisabledButton(true)
-    setTimeout(() => setDisabledButton(false), 1000)
-  }
+const Home = async () => {
+  const furnitures = await prisma.furniture.findMany()
 
-  const isGallery = currentContent === 'gallery'
-  const isAbout = currentContent === 'about'
-
-  return (
-    <main>
-      <FixedContentWrapper
-        hideContent={!isGallery}
-        className={cn(
-          !initialState && (isGallery ? 'animate-gallery-move-right' : 'animate-gallery-move-left'),
-        )}
-      >
-        <Gallery />
-      </FixedContentWrapper>
-      <FixedContentWrapper
-        hideContent={!isAbout}
-        className={cn(
-          initialState
-            ? 'translate-x-[100%]'
-            : (isAbout ? 'animate-about-move-left' : 'animate-about-move-right'),
-        )}
-      >
-        <About />
-      </FixedContentWrapper>
-      <SwitchContentButton
-        onClick={() => {
-          disableButton()
-          setInitialState(false)
-          setCurrentContent(currentContent === 'gallery' ? 'about' : 'gallery')
-        }}
-        disabled={disabledButton}
-        className={cn(
-          !initialState && (isGallery ? 'animate-rotate-180-back' : 'animate-rotate-180'),
-        )}
-      />
-    </main>
-  )}
+  return <MainContent furnitures={furnitures} />
+}
 
 export default Home
---
+++ src/components/main_content.tsx
@@ -0,0 +1,61 @@
+'use client'
+
+import * as React from 'react'
+
+import { About } from '../components/about'
+import { FixedContentWrapper } from '../components/fixed_content_wrapper'
+import { Gallery } from '../components/gallery'
+import { SwitchContentButton } from '../components/switch_content_button'
+import { cn } from '../utils/tailwind'
+
+import type { Furniture } from '@prisma/client'
+
+type Content = 'gallery' | 'about'
+
+export const MainContent = ({ furnitures }: { furnitures: Furniture[] }) => {
+  const [initialState, setInitialState] = React.useState(true)
+  const [currentContent, setCurrentContent] = React.useState<Content>('gallery')
+  const [disabledButton, setDisabledButton] = React.useState(false)
+
+  const disableButton = () => {
+    setDisabledButton(true)
+    setTimeout(() => setDisabledButton(false), 1000)
+  }
+
+  const isGallery = currentContent === 'gallery'
+  const isAbout = currentContent === 'about'
+
+  return (
+    <main>
+      <FixedContentWrapper
+        hideContent={!isGallery}
+        className={cn(
+          !initialState && (isGallery ? 'animate-gallery-move-right' : 'animate-gallery-move-left'),
+        )}
+      >
+        <Gallery furnitures={furnitures} />
+      </FixedContentWrapper>
+      <FixedContentWrapper
+        hideContent={!isAbout}
+        className={cn(
+          initialState
+            ? 'translate-x-[100%]'
+            : (isAbout ? 'animate-about-move-left' : 'animate-about-move-right'),
+        )}
+      >
+        <About />
+      </FixedContentWrapper>
+      <SwitchContentButton
+        onClick={() => {
+          disableButton()
+          setInitialState(false)
+          setCurrentContent(currentContent === 'gallery' ? 'about' : 'gallery')
+        }}
+        disabled={disabledButton}
+        className={cn(
+          !initialState && (isGallery ? 'animate-rotate-180-back' : 'animate-rotate-180'),
+        )}
+      />
+    </main>
+  )
+}
