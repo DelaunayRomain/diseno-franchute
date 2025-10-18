ff355c35dd10b9034b3a888be57d5922a3a302e6
DelaunayRomain <disenofranchute@gmail.com>
2025-10-18 16:27:51 -0300

    prevent horizontal scrolling in IOS

--- src/components/fixed_content_wrapper.tsx
+++ src/components/fixed_content_wrapper.tsx
@@ -11,10 +11,18 @@ export const FixedContentWrapper = ({
   hideContent,
   className,
 }: FixedContentWrapperProps) => (
-  <div className={cn('fixed inset-0', hideContent && '-z-10')}>
-    <div className='h-full overflow-y-auto overscroll-contain scroll-smooth no-scrollbar'>
-      <div className={className}>
-        {children}
+  <div
+    className={cn(
+      'fixed inset-0 h-full',
+      'overflow-y-auto overscroll-contain scroll-smooth no-scrollbar scroll-touch',
+      hideContent && '-z-10',
+    )}
+  >
+    <div className='relative w-full max-w-full min-w-0 overflow-hidden'>
+      <div className='w-full max-w-full min-w-0'>
+        <div className={className}>
+          {children}
+        </div>
       </div>
     </div>
   </div>
